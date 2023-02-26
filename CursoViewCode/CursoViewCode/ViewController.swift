583bf2bc0f8a493dc90b1b68af9d37f4db70d6c2
wmilani <milani.weslley@live.com>
2023-02-25 21:42:28 -0300

    Aula 5, criacao de repositorio

---
+++ CursoViewCode/CursoViewCode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CursoViewCode
+//
+//  Created by Weslley Milani on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+
+    }
+
+
+}
+
